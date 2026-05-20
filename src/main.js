a67f0da0941bfeed9e7ce84a9d0b500b5adea80d
Vova Bernevek <vovabernevek@gmail.com>
2026-05-20 07:59:55 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import App from './App.vue'
 import posthog from 'posthog-js'
 
 posthog.init('phc_ARXkGMBPGeBmDuXMGMp5v8EokkQ85wgBTnR4MUth8q9c', {
-  api_host: 'https://us.posthog.com',
-  person_profiles: 'always'
+  api_host: 'https://us.i.posthog.com', 
+  
+  // Додатково підказуємо системі, де знаходиться ваш веб-кабінет
+  ui_host: 'https://app.posthog.com',
+  
+  person_profiles: 'always',
+  
+  // Примусово кажемо клієнту запускати запис
+  disable_session_recording: false 
 })
 
 createApp(App).mount('#app')
